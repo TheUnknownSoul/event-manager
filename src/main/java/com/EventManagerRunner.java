43044dacfa3efac6507ec085218334f9e6eca57b
Andrei Roi <andriyroy3@gmail.com>
2021-05-25 11:59:34 +0300

    feature:#3/event-manager delete unused import

--- src/main/java/com/EventManagerRunner.java
+++ src/main/java/com/EventManagerRunner.java
@@ -5,10 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Slf4j
-@EnableMongoRepositories(basePackages = "com.repository")
 @SpringBootApplication(exclude ={DataSourceAutoConfiguration.class } )
 public class EventManagerRunner {
 
@@ -17,7 +15,4 @@ public class EventManagerRunner {
         log.info("Application starts");
         SpringApplication.run(EventManagerRunner.class);
     }
-
-
-
 }
